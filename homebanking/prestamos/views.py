d787db40d89f3328dde05090f06aa892db2fc169
faelias20 <fexeelias@gmail.com>
2022-08-12 23:12:24 -0300

    add user permissions

--- homebanking/prestamos/views.py
+++ homebanking/prestamos/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Project
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def prestamos(request):
     projects = Project.objects.all()
     return render(request,'prestamos/prestamos.html')
--- homebanking/tarjetas/views.py
+++ homebanking/tarjetas/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Project
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def tarjetas(request):
     projects = Project.objects.all()
     return render(request,'tarjetas/tarjetas.html')
