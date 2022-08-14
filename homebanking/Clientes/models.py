3037b77902635665977c5b9d5fe3ffc3e80ae0ee
LautaroProfumo <prolaucha8@gmail.com>
2022-08-14 15:14:17 -0300

    models

--- homebanking/Clientes/models.py
+++ homebanking/Clientes/models.py
@@ -13,8 +13,7 @@ class Project(models.Model):    #Hereda de Django para obtener funcionalidad que
         from django.db import models
 
 class clientes(models.Model):
-        customer_id=models.IntegerField()
-        name=models.TextField(max_length=40)
+       name=models.TextField(max_length=40)
         surname=models.TextField(max_length=25)
         dni=models.IntegerField()
         direccion=models.TextField(max_length=100)
@@ -22,13 +21,11 @@ class clientes(models.Model):
 
 class cuenta(models.Model):
         account_id=models.IntegerField()
-        customer_id=models.IntegerField()
         balance=models.IntegerField()
         iban=models.TextField(max_length=100)
         tipo_de_cuenta_id=models.IntegerField()
 
 class empleado(models.Model):
-        employee_id=models.IntegerField()
         employee_name=models.TextField(max_length=100)
         employee_surname=models.TextField(max_length=100)
         employee_hire_date=models.TextField(max_length=300)
@@ -37,21 +34,17 @@ class empleado(models.Model):
         direccion_id=models.IntegerField()
 
 class movimientos(models.Model):
-        id_movimiento=models.IntegerField()
         iban=models.TextField(max_length=100)
         monto=models.IntegerField()
         tipo_operacion=models.TextField(max_length=200)
         hora=models.DateTimeField()
 
 class prestamos(models.Model):
-        loan_id=models.IntegerField()
         loan_type=models.TextField(max_length=200)
         loan_date=models.TextField(max_length=500)
         loan_total=models.IntegerField()
-        customer_id=models.IntegerField()
 
 class tarjetas(models.Model):
-        tarjeta_id=models.IntegerField()
         numero_tarjeta=models.IntegerField()
         cvv=models.IntegerField()
         fecha_emision=models.TextField(max_length=200)
