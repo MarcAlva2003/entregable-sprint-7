020dde837b50112f2fa37969e24be109df99acd6
OjedaP <ojedapaulo97@gmail.com>
2022-08-14 19:48:51 -0300

    indentacion models cliente

--- homebanking/Clientes/models.py
+++ homebanking/Clientes/models.py
@@ -13,7 +13,7 @@ class Project(models.Model):    #Hereda de Django para obtener funcionalidad que
         from django.db import models
 
 class clientes(models.Model):
-       name=models.TextField(max_length=40)
+        name=models.TextField(max_length=40)
         surname=models.TextField(max_length=25)
         dni=models.IntegerField()
         direccion=models.TextField(max_length=100)
