5ab7931ed0370083b6562e64cf6f0189f4b5e95c
OjedaP <ojedapaulo97@gmail.com>
2022-08-15 15:00:08 -0300

    formulario prestamos funciona

--- homebanking/prestamos/views.py
+++ homebanking/prestamos/views.py
@@ -11,6 +11,8 @@ from datetime import datetime
 @login_required
 def prestamos(request):
 
+    prestamos_db = Prestamos.objects.filter(id_cliente__icontains = request.user.id)
+
     helper = clientes.objects.filter(id__icontains = request.user.id)
     user_client_type = helper[0].tipo_cliente
     print(helper[0].tipo_cliente)
@@ -32,20 +34,15 @@ def prestamos(request):
                 print('BLACK MAYOR')
                 return redirect(reverse('prestamos') + "?errorblack")
             else:
-                print('IF 2')
-                return redirect(reverse('prestamos') + "?ok")
+                #print('IF 2')
+                #return redirect(reverse('prestamos') + "?ok")
+                money_amount = int(request.POST.get('moneyAmount'))
+                monthsAmount_received = int(request.POST.get('monthsAmount'))
+                loanType_received = request.POST.get('loanType')
+                id_cliente_received = request.user.id
+                prestamo = Prestamos(loan_approved_date=datetime.now(),loan_month = monthsAmount_received,loan_total=money_amount,loanType = loanType_received,id_cliente = id_cliente_received)
+                prestamo.save()
 
-    prestamos = Prestamos.objects.filter(id__icontains = request.user.id)
-
-    if loan_form.is_valid():
-        money_amount = int(request.POST.get('moneyAmount'))
-        monthsAmount_received = int(request.POST.get('monthsAmount'))
-        loanType_received = int(request.POST.get('loanType'))
-        id_cliente_received = int(id__icontains = request.user.id)
-        prestamo = Prestamos.objects.create(loan_approved_date=datetime.now(),loan_month = monthsAmount_received,loan_total=money_amount,loanType = loanType_received,id_cliente = id_cliente_received)
-        prestamo.save()
-        return redirect(reverse('prestamos')+ "?ok")
-
-    return render(request,'prestamos/prestamos.html', {'prestamos':prestamos,'form':loan_form, 'client_type':user_client_type})
+    return render(request,'prestamos/prestamos.html', {'prestamos_db':prestamos_db,'form':loan_form, 'client_type':user_client_type})
 
         # print(request.user.username)
