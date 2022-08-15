b62b8e90f78c2aac9bf5aab08b0500d8c6202ede
OjedaP <ojedapaulo97@gmail.com>
2022-08-14 22:05:35 -0300

    prestamos en proceso

--- homebanking/prestamos/views.py
+++ homebanking/prestamos/views.py
@@ -4,7 +4,8 @@ from .models import Project
 from .forms import LoanForm
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-
+from Clientes.models import prestamos as Prestamos
+from datetime import datetime
 
 # Create your views here.
 @login_required
@@ -18,7 +19,7 @@ def prestamos(request):
         
         if loan_form.is_valid():
             money_amount = int(request.POST.get('moneyAmount'))
-            print()
+            
             if (user_client_type == 'classic') and (money_amount > 100000):
                 print('CLASSIC MAYOR')
                 return redirect(reverse('prestamos') + "?amounterror")
@@ -32,7 +33,15 @@ def prestamos(request):
                 print('IF 2')
                 return redirect(reverse('prestamos') + "?ok")
 
-    projects = Project.objects.all()
-    return render(request,'prestamos/prestamos.html', {'form':loan_form, 'client_type':user_client_type })
+    projects = Project.objects.all() #Hay que hacer un .get filtrando por id de cliente y mostrar todos los prestamos
+    #a la data que obtengo de aca hay que mostrarla en el content superior
+    if loan_form.is_valid():
+        money_amount = int(request.POST.get('moneyAmount'))
+        monthsAmount_received = int(request.POST.get('monthsAmount'))
+        loanType_received = int(request.POST.get('loanType'))
+        prestamo = Prestamos(loan_approved_date=datetime.now(),loan_month = monthsAmount_received,loan_total=money_amount,loanType = loanType_received)
+        prestamo.save()
+
+    return render(request,'prestamos/prestamos.html', {'form':loan_form, 'client_type':user_client_type})
 
         # print(request.user.username)
