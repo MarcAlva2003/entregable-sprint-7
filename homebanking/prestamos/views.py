6386f05ca12d7c71f9252ff91a540fcf5966cd38
OjedaP <ojedapaulo97@gmail.com>
2022-08-15 13:41:01 -0300

    cambios en db prestamos y views

--- homebanking/prestamos/views.py
+++ homebanking/prestamos/views.py
@@ -10,17 +10,12 @@ from datetime import datetime
 # Create your views here.
 @login_required
 def prestamos(request):
-<<<<<<< HEAD
 
-    helper = clientes.objects.values('tipo_cliente')[0]
-    user_client_type = 'classic'
-    print(helper)
-=======
     helper = clientes.objects.filter(id__icontains = request.user.id)
     user_client_type = helper[0].tipo_cliente
     print(helper[0].tipo_cliente)
->>>>>>> c060f30ff376d53d2e19e0e36200146c2596b2f1
     loan_form = LoanForm()
+
     if request.method == "POST":
         loan_form = LoanForm(data=request.POST)
         
@@ -40,15 +35,17 @@ def prestamos(request):
                 print('IF 2')
                 return redirect(reverse('prestamos') + "?ok")
 
-    prestamos_db = Prestamos.objects.all() #Hay que hacer un .get filtrando por id de cliente y mostrar todos los prestamos
-    #a la data que obtengo de aca hay que mostrarla en el content superior
+    prestamos = Prestamos.objects.filter(id__icontains = request.user.id)
+
     if loan_form.is_valid():
         money_amount = int(request.POST.get('moneyAmount'))
         monthsAmount_received = int(request.POST.get('monthsAmount'))
         loanType_received = int(request.POST.get('loanType'))
-        prestamo = Prestamos.objects.create(loan_approved_date=datetime.now(),loan_month = monthsAmount_received,loan_total=money_amount,loanType = loanType_received)
-        #prestamo.save()
-        #return redirect('/',{'prestamos_db':prestamos_db,'form':loan_form, 'client_type':user_client_type})
-    return render(request,'prestamos/prestamos.html', {'prestamos_db':prestamos_db,'form':loan_form, 'client_type':user_client_type})
+        id_cliente_received = int(id__icontains = request.user.id)
+        prestamo = Prestamos.objects.create(loan_approved_date=datetime.now(),loan_month = monthsAmount_received,loan_total=money_amount,loanType = loanType_received,id_cliente = id_cliente_received)
+        prestamo.save()
+        return redirect(reverse('prestamos')+ "?ok")
+
+    return render(request,'prestamos/prestamos.html', {'prestamos':prestamos,'form':loan_form, 'client_type':user_client_type})
 
         # print(request.user.username)
